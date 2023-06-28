2e3a2048a4fe1ba6860959507f6c0660ebf40415
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:54:41 +0530

    [ADD] Added BookShelf model

---
+++ ReadRate/Models/BookShelf.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class BookShelf
+    {
+        public int BookShelfId { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public string ReadingStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ ReadRate/Models/RatingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class RatingModel
+    {
+        public int RatingId { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

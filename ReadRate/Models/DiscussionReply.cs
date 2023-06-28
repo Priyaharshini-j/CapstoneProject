2d5ae22cf31dd2d9fef75a9c64fc29e9fe3123d5
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:59:55 +0530

    [MODIFY] Modified an item in the model

--- ReadRate/Models/DiscussionReply.cs
+++ ReadRate/Models/DiscussionReply.cs
@@ -5,7 +5,7 @@
         public int DiscussionReplyId { get; set; }
         public int DiscussionId { get; set; }
         public int CommunityMemberId { get; set; }
-        public string DiscussionReply { get; set; }
+        public string Reply { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }

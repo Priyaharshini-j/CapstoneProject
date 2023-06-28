1fc294685a317112bc40ea1ba89a92d41f482b1e
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 20:02:46 +0530

    [ADD] Added CritiqueLike Model

---
+++ ReadRate/Models/CritiqueLike.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class CritiqueLike
+    {
+        public int CritiqueLikeId { get; set; }
+        public int CritiqueId { get; set; }
+        public int UserId { get; set; }
+        public bool LikeStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

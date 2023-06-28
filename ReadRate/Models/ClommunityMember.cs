f60d612a627cbd3bbb108e36c5bf5a14b004e44a
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:55:39 +0530

    [ADD] Added CommunityMember Model

---
+++ ReadRate/Models/ClommunityMember.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadRate.Models
+{
+    public class ClommunityMember
+    {
+        public int CommunityMemberId { get; set; }
+        public int CommunityId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

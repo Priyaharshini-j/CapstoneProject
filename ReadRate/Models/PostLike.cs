992838f36efb1fcf93ce1fb75d8050e55d30bc24
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 20:00:49 +0530

    [ADD] Added PostLike Model

---
+++ ReadRate/Models/PostLike.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class PostLike
+    {
+        public int LikeId { get; set; }
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public bool LikeStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

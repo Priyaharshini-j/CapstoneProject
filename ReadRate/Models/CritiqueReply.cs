9426f7a50afef3f32f636f33af2cac43a2c8d043
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 20:01:39 +0530

    [ADD] Added CritiqueReply Model

---
+++ ReadRate/Models/CritiqueReply.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class CritiqueReply
+    {
+        public int CritiqueReplyId { get; set; }
+        public int CritiqueId { get; set; }
+        public int UserId { get; set; }
+        public string Reply { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

829f6b9254e95e4b4effa3c5c515096be98107b8
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:51:22 +0530

    [ADD] Added the Post Model

---
+++ ReadRate/Models/PostModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadRate.Models
+{
+    public class PostModel
+    {
+        public int PostId { get; set; }
+        public string PostCaption { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

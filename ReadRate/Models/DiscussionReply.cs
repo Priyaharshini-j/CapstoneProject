1ee564bb13981f09373c449aa7c168d8f01c520b
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:59:27 +0530

    [ADD] Added DiscussionReply model

---
+++ ReadRate/Models/DiscussionReply.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class DiscussionReply
+    {
+        public int DiscussionReplyId { get; set; }
+        public int DiscussionId { get; set; }
+        public int CommunityMemberId { get; set; }
+        public string DiscussionReply { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

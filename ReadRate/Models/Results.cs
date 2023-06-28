325bc526e88a73d091c9e695d80fecb02257a2f9
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 18:36:08 +0530

    [MODIFY] Added Results model

---
+++ ReadRate/Models/Results.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadRate.Models
+{
+    public class Results
+    {
+        public bool result { get; set; }
+        public string message { get; set; }
+    }
+}
--- ReadRate/Models/UserModel.cs
+++ ReadRate/Models/UserModel.cs
@@ -21,9 +21,4 @@ namespace ReadRate.Models
 
     }
 
-    public class Results
-    {
-        public bool result { get; set; }
-        public string message { get; set; }
-    }
 }

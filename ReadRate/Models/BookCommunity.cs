94893f9cf9b24f626326c17ac1ffd1cad6c68743
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:50:15 +0530

    [UPDATE] Updated the BookCommunity model.

--- ReadRate/Models/BookCommunity.cs
+++ ReadRate/Models/BookCommunity.cs
@@ -2,6 +2,11 @@
 {
     public class BookCommunity
     {
-
+        public int CommunityId { get; set; }
+        public string CommunityName { get; set; }
+        public string CommunityDesc { get; set; }
+        public int CommunityAdmin { get; set; }
+        public int BookId { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }

ab7e3ae81f612e28017ac30e49f3acdbbe2cd84f
Priyadharshini J <priyadj2001@gmail.com>
2023-06-28 19:58:01 +0530

    [ADD] Added the CommunityMember Model

---
+++ ReadRate/Models/CommunityDiscussion.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadRate.Models
+{
+    public class CommunityDiscussion
+    {
+        public int DiscussionId { get; set; }
+        public int CommunityId { get; set; }
+        public int CommunityMemberId { get; set; }
+        public string Discussion { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
